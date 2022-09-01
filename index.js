10d7b1ab0a77e068194125fefe9d588a4460b272
Tanim Ismat <tanim.ismat@gmail.com>
2022-09-02 00:48:56 +0600

    get/user endpoint

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const fs = require("fs");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.send("My random user api!!");
+app.get("/user", (req, res) => {
+	fs.readFile("user.json", (err, data) => {
+		if (err) {
+			res.write("Failed to load data!");
+			res.end();
+		} else {
+			res.write(data);
+			res.end();
+		}
+	});
 });
 
 app.listen(port, () => {
