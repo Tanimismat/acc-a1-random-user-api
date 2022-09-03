fada4864adba33d67f586f9ac854d84a9cf114a2
Tanim Ismat <tanim.ismat@gmail.com>
2022-09-03 21:11:13 +0600

    /user/random endpoint

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
-
-// console.log(fs);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -16,11 +14,42 @@ app.get("/user", (req, res) => {
 			res.end();
 		} else {
 			res.write(data);
-			// console.log(data);
 			res.end();
 		}
 	});
 });
+
+function getRandomUser(user) {
+	const randomIndex = Math.floor(Math.random() * user.length);
+	const randomUser = user[randomIndex];
+	return randomUser;
+}
+
+app.get("/user/random", (req, res) => {
+	if ((req.url = "/user/random")) {
+		fs.readFile("data.json", (err, data) => {
+			if (err) {
+				res.write("Failed to load a random user!");
+				res.end();
+			} else {
+				console.log(data);
+				let user = getRandomUser(data);
+				console.log(user);
+				res.writeHead(200, { "Content-Type": "application/json" });
+				res.write(JSON.stringify(user));
+				res.end();
+			}
+		});
+	}
+});
+
+// var http = require("http");
+
+// const server = http.createServer(function (req, res) {
+// 	//write code here
+// });
+
+// server.listen(5000);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
