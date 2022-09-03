bbcca0fa60e77693887a735b72aeb32b2ed899e4
Tanim Ismat <tanim.ismat@gmail.com>
2022-09-03 18:42:26 +0600

    user data

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fs = require("fs");
+
+// console.log(fs);
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/user", (req, res) => {
-	fs.readFile("user.json", (err, data) => {
+	fs.readFile("data.json", (err, data) => {
 		if (err) {
 			res.write("Failed to load data!");
 			res.end();
 		} else {
 			res.write(data);
+			// console.log(data);
 			res.end();
 		}
 	});
