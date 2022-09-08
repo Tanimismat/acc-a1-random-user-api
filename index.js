077c8e4f440f322f4aa6fd1e96050c856287be59
Tanim Ismat <tanim.ismat@gmail.com>
2022-09-08 23:16:05 +0600

    GET/user/all route

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ const cors = require("cors");
 const fs = require("fs");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
-app.get("/user", (req, res) => {
+app.get("/user/all", (req, res) => {
 	fs.readFile("data.json", (err, data) => {
 		if (err) {
 			res.write("Failed to load data!");
 			res.end();
 		} else {
+			// const { limit, page } = req.query;
+			// console.log(limit, page);
+			// res.json(data.slice(0, limit));
 			res.write(data);
 			res.end();
 		}
@@ -36,20 +39,12 @@ app.get("/user/random", (req, res) => {
 				let user = getRandomUser(data);
 				console.log(user);
 				res.writeHead(200, { "Content-Type": "application/json" });
-				res.write(JSON.stringify(user));
+				res.write(JSON.stringify(user.toString()));
 				res.end();
 			}
 		});
 	}
 });
-
-// var http = require("http");
-
-// const server = http.createServer(function (req, res) {
-// 	//write code here
-// });
-
-// server.listen(5000);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
