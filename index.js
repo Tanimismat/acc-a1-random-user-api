d3ae8dc5d0ce64b394710af2ff1ea5f45bbafa0e
Tanim Ismat <tanim.ismat@gmail.com>
2022-09-09 23:41:57 +0600

    can add a user

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+let user = [
+	{
+		id: "1",
+		photoUrl: "http://placehold.it/32x32",
+		name: "Maryanne Madden",
+		gender: "female",
+		contact: "+1 (918) 530-2617",
+		address: "100 Sands Street, Kanauga, Puerto Rico, 1525",
+	},
+];
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -29,21 +40,38 @@ function getRandomUser(user) {
 }
 
 app.get("/user/random", (req, res) => {
-	if ((req.url = "/user/random")) {
-		fs.readFile("data.json", (err, data) => {
-			if (err) {
-				res.write("Failed to load a random user!");
-				res.end();
-			} else {
-				console.log(data);
-				let user = getRandomUser(JSON.parse(data));
-				console.log(user);
-				// res.writeHead(200, { "Content-Type": "application/json" });
-				res.write(JSON.stringify(user));
-				res.end();
-			}
-		});
-	}
+	// if ((req.url = "/user/random")) {
+	fs.readFile("data.json", (err, data) => {
+		if (err) {
+			res.write("Failed to load a random user!");
+			res.end();
+		} else {
+			console.log(data);
+			let user = getRandomUser(JSON.parse(data));
+			console.log(user);
+			// res.writeHead(200, { "Content-Type": "application/json" });
+			res.write(JSON.stringify(user));
+			res.end();
+		}
+	});
+});
+
+app.post("/user/save", (req, res) => {
+	console.log(req.body);
+
+	let data = JSON.stringify(user);
+	fs.appendFile("data.json", data, (err) => {
+		if (err) {
+			res.write("Failed to save user!");
+			res.end();
+		} else {
+			user.push(data);
+			res.send(user);
+			// res.write("data written successfully");
+			// res.end();
+		}
+	});
+	// res.send("sending data");
 });
 
 app.listen(port, () => {
